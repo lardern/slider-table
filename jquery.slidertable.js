dc0b91f8ec45ffda14f244092eaff34136da9def
lardern <bomwubomwu@gmail.com>
2017-09-17 21:53:58 +0800

    change animation

--- jquery.slidertable.js
+++ jquery.slidertable.js
@@ -7,11 +7,9 @@
         var config = {
             id: 'st' + (Math.random() * 1000).toFixed(0),//指定table id，可以据此ID写CSS
             rows: 5, //行数
-            height: 300,//表格高度
-            headHeight: 30,//表头高度
+            showHead: true,
             interval: 3000,//轮播间隔，ms
             speed: 1000,//动画速度,ms
-            url: null,//
             data: [{id: 1, name: '中国'}, {id: 2, name: '中国'}, {id: 3, name: '中国'}, {id: 4, name: '中国'}, {
                 id: 5,
                 name: '中国'
@@ -19,10 +17,26 @@
                 id: 10,
                 name: '中国'
             }, {id: 11, name: '中国'}, {id: 12, name: '中国'}], /*取数地址{String}，或 callback函数{Function},或静态数据{Array}*/
-            columns: [{dataIndex: 'id', text: '序号', flex: 1}, {dataIndex: 'name', text: '国家', flex: 2}]//列配置
+            columns: [{dataIndex: 'id', text: '序号', flex: 1}, {dataIndex: 'name', text: '国家', flex: 2}],//列配置
+            rowRender: function (el, columns, data, rowHeight, cls) {
+                cls = cls || "";
+                var html = ['<div class="tr ' + cls + '" style="height: ' + rowHeight + 'px">'];
+                for (var j = 0; j < columns.length; j++) {
+                    if (columns[j].columnRender) {
+                        html.push(columns[j].columnRender(el, columns[j], data));
+                    } else {
+                        html.push(this.columnRender(el, columns[j], data));
+                    }
+                }
+                html.push('</div>');
+                return html.join('');
+            },
+
+            columnRender: function (el, column, data) {
+                return '<div class="td text-' + (column.align ? column.align : 'center') + '" style="width:' + column.width + '">' + (data[column.dataIndex] == null ? "" : data[column.dataIndex]) + '</div>';
+            }
         };
         var interval = null;
-        var page;
         if (options) {
             $.extend(config, options);
         }
@@ -30,9 +44,10 @@
             var html = [];
             var sumFlex = 0;
             var elHeight = $(el).height();
-            var rowHeight = elHeight / (config.rows + 1);
+            var rowHeight = elHeight / (config.showHead ? (config.rows + 1) : config.rows);
             html.push('<div id="' + config.id + '" class="table" style="height:' + elHeight + 'px">');
-            html.push('<div class="thead">');
+            html.push('<div class="thead" style="display:' + (config.showHead ? 'block' : 'none') + ';">');
+
             $.each(config.columns, function (index, column) {
                 sumFlex += (column.flex ? column.flex : 1);
             });
@@ -40,61 +55,70 @@
             html.push('<div class="tr" style="height: ' + rowHeight + 'px">');
             $.each(config.columns, function (index, column) {
                 column.width = ((column.flex ? column.flex : 1) * 100 / sumFlex) + '%';
-                html.push('<div class="th" style="width:' + column.width + '">' + column.text + '</div>');
+                html.push('<div class="th" style="width:' + column.width + '">' + (column.text == null ? "" : column.text) + '</div>');
             });
             html.push('</div>');
             html.push('</div>');// thead end
 
             html.push('<div class="tbody">');
-            for (var i = 0; i < data.length; i++) {
-                if (config.rowRender) {
-                    html.push(config.rowRender(el, config.columns, data[i], rowHeight));
-                } else {
-                    html.push(rowRender(el, config.columns, data[i], rowHeight));
-                }
+            var point = 0;
+            for (var i = 0; i < config.rows; i++) {
+                var cls = i % 2 ? 'odd' : 'eve';
+                html.push(config.rowRender(el, config.columns, data[i], rowHeight, cls));
+                point = i;
             }
             html.push('</div>');//tbody end
             html.push('</div>'); //table end
             el.append(html.join(''));
+            el.find('.tbody .tr:first').css({
+                'margin-top': -rowHeight + 'px'
+            });
+            el.find('.tbody').css({
+                'padding-top': rowHeight + 'px'
+            });
             if (config.interval) {
-                var totalPage = Math.ceil(data.length / config.rows);
-                var page = 0;
                 interval = setInterval(function () {
-                    var start = page * config.rows;
-                    var end = page * config.rows + config.rows - 1;
-                    if (start > 0) {
-                        el.find('.tbody .tr:lt(' + (start) + ')').animate({height: 0});
+                    point = point + 1;
+                    if (point >= data.length) {
+                        point = 0;
                     }
-                    el.find('.tbody .tr:gt(' + (end) + ')').animate({height: 0});
-                    if (start > 0) {
-                        el.find('.tbody .tr:gt(' + (start - 1) + '):lt(' + (end + 1) + ')').animate({height: rowHeight});
-                    } else {
-                        el.find('.tbody .tr:lt(' + (end + 1) + ')').animate({height: rowHeight});
-                    }
-                    page++;
-                    if (page >= totalPage) {
-                        page = 0;
-                    }
+                    var tr = config.rowRender(el, config.columns, data[point], rowHeight, (point % 2 ? 'odd' : 'eve'));
+                    //el.find('.tbody .tr:first').animate({marginTop: -rowHeight}, config.speed, "swing", function () {
+                    //    this.remove();
+                    //});
+                    el.find('.tbody .tr:first').animate({marginTop: rowHeight*-2}, config.speed, "swing", function () {
+                        this.remove();
+                        el.find('.tbody .tr:first').css({
+                            'margin-top': -rowHeight + 'px'
+                        });
+                        //el.find('.tbody .tr').removeClass('eve').removeClass('odd');
+                        //el.find('.tbody .tr:even').addClass('eve');
+                        //el.find('.tbody .tr:odd').addClass('odd');
+                    });
+                    el.find('.tbody').append(tr);
                 }, config.interval);
+                //var totalPage = Math.ceil(data.length / config.rows);
+                //var page = 0;
+                //interval = setInterval(function () {
+                //    var start = page * config.rows;
+                //    var end = page * config.rows + config.rows - 1;
+                //    if (start > 0) {
+                //        el.find('.tbody .tr:lt(' + (start) + ')').animate({height: 0});
+                //    }
+                //    el.find('.tbody .tr:gt(' + (end) + ')').animate({height: 0});
+                //    if (start > 0) {
+                //        el.find('.tbody .tr:gt(' + (start - 1) + '):lt(' + (end + 1) + ')').animate({height: rowHeight});
+                //    } else {
+                //        el.find('.tbody .tr:lt(' + (end + 1) + ')').animate({height: rowHeight});
+                //    }
+                //    page++;
+                //    if (page >= totalPage) {
+                //        page = 0;
+                //    }
+                //}, config.interval);
             }
         }
 
-        function rowRender(el, columns, data, rowHeight) {
-            var html = ['<div class="tr" style="height: ' + rowHeight + 'px">'];
-            for (var j = 0; j < columns.length; j++) {
-                if (columns[j].columnRender) {
-                    html.push(columns[j].columnRender(el, columns[j], data));
-                } else {
-                    html.push(columnRender(el, columns[j], data));
-                }
-            }
-            html.push('</div>');
-            return html.join('');
-        }
-
-        function columnRender(el, column, data) {
-            return '<div class="td text-' + (column.align ? column.align : 'center') + '" style="width:' + column.width + '">' + data[column.dataIndex] + '</div>';
-        }
 
         return this.each(function () {
             var el = $(this);
